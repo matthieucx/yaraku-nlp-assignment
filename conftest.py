ec86dddd484999c620b4a70c004a100507855ac0
Matthieu <matthieu.cx@gmail.com>
2023-12-12 19:05:42 +0100

    Introduce logging with Loguru
    - Update dependencies
    - Override a fixture to use Loguru with pytest

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from loguru import logger
+from _pytest.logging import LogCaptureFixture
+
+
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture):
+    """This fixture is used to capture logs from the logger.
+
+    Override the default pytest caplog fixture to use the loguru logger.
+
+    Source:
+    `Loguru documentation
+    <https://loguru.readthedocs.io/en/stable/resources/migration.html#replacing-caplog-fixture-from-pytest-library>`_
+
+    """
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        # Set to 'True' if your test is spawning child processes.
+        enqueue=False,
+    )
+    yield caplog
+    logger.remove(handler_id)
